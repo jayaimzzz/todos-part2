83bffc87f5e1e63564b8c9a52ca78435355396bb
jayaimzzz <jameshorton@gmail.com>
2018-11-19 15:24:55 -0500

    Updates

---
+++ precache-manifest.dbd358bfa4404994c3fa25942a563777.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1140f88ee7514257d134",
+    "url": "/todos-part2/static/css/main.aa703a9d.chunk.css"
+  },
+  {
+    "revision": "1140f88ee7514257d134",
+    "url": "/todos-part2/static/js/main.1140f88e.chunk.js"
+  },
+  {
+    "revision": "4117986fb66a4ee8335a",
+    "url": "/todos-part2/static/js/1.4117986f.chunk.js"
+  },
+  {
+    "revision": "6fa746ac323e42cf0e35",
+    "url": "/todos-part2/static/js/runtime~main.6fa746ac.js"
+  },
+  {
+    "revision": "89a9dbaa9a2362c73672e9f59a8d5a8b",
+    "url": "/todos-part2/index.html"
+  }
+];
